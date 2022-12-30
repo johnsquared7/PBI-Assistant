b04e1f63be7831c9e712eb81963c5dc7a00251bb
johnsquared7 <119292884+johnsquared7@users.noreply.github.com>
2022-12-30 21:49:01 +0000

    Undo

--- pages/index.js
+++ pages/index.js
@@ -85,9 +85,8 @@ const callGenerateEndpoint = async () => {
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>Built by John with buildspace</p>
           </div>
-          <div style="position: absolute; bottom: 0; right: 0; color: white;">built by John</div>
         </a>
       </div>
     </div>
