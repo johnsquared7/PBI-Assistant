ef831d766b53c7206298d90de65c80d56cbbbe50
johnsquared7 <119292884+johnsquared7@users.noreply.github.com>
2022-12-30 22:00:15 +0000

    Adding proper title

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="AI DAX Assistant " key="title"/>
+        <meta property="og:description" content="GPT 3 Assistant built with buildspace" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://images.techyscouts.media/cb:e7Py~323d8/w:auto/h:auto/q:mauto/https://senturus.com/app/uploads/2019/12/DAX-function-Updated.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
