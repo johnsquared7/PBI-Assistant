1dcca35421075bd923b626faaaaac57239bddbf0
John <jojohnson250@gmail.com>
2023-03-30 10:01:00 +0100

    changed to chat model

---
+++ src/routes/api/chat/+server.ts
@@ -0,0 +1,102 @@
+import { OPENAI_KEY } from '$env/static/private'
+import type { CreateChatCompletionRequest, ChatCompletionRequestMessage } from 'openai'
+import type { RequestHandler } from './$types'
+import { getTokens } from '$lib/tokenizer'
+import { json } from '@sveltejs/kit'
+import type { Config } from '@sveltejs/adapter-vercel'
+
+export const config: Config = {
+	runtime: 'edge'
+}
+
+export const POST: RequestHandler = async ({ request }) => {
+	try {
+		if (!OPENAI_KEY) {
+			throw new Error('OPENAI_KEY env variable not set')
+		}
+
+		const requestData = await request.json()
+
+		if (!requestData) {
+			throw new Error('No request data')
+		}
+
+		const reqMessages: ChatCompletionRequestMessage[] = requestData.messages
+
+		if (!reqMessages) {
+			throw new Error('no messages provided')
+		}
+
+		let tokenCount = 0
+
+		reqMessages.forEach((msg) => {
+			const tokens = getTokens(msg.content)
+			tokenCount += tokens
+		})
+
+		const moderationRes = await fetch('https://api.openai.com/v1/moderations', {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${OPENAI_KEY}`
+			},
+			method: 'POST',
+			body: JSON.stringify({
+				input: reqMessages[reqMessages.length - 1].content
+			})
+		})
+		if (!moderationRes.ok) {
+			const err = await moderationRes.json()
+			throw new Error(err.error.message)
+		}
+
+		const moderationData = await moderationRes.json()
+		const [results] = moderationData.results
+
+		if (results.flagged) {
+			throw new Error('Query flagged by openai')
+		}
+
+		const prompt =
+			'You are an assistant designed to help write Power BI DAX or help users understand what a DAX function does. You will write DAX in the correct syntax for PowerBI with the correct formatting or explain what the function does with an example that has DAX code. Example 1: What does the DAX function SUMX do in Power BI. The SUMX DAX function in Power BI is used to calculate the sum of an expression over a table or set. It iterates over the table or set and evaluates the specified expression for each row and returns the sum of all expressions. This function is especially useful when you are working with complex calculations.Example 2: Write dax to calculate number of sales last month. CALCULATE(COUNT(Sales[Sales]), DATESINPERIOD(Calendar[Date], LASTDATE(Calendar[Date]), -1, MONTH))'
+		tokenCount += getTokens(prompt)
+
+		if (tokenCount >= 4000) {
+			throw new Error('Query too large')
+		}
+
+		const messages: ChatCompletionRequestMessage[] = [
+			{ role: 'system', content: prompt },
+			...reqMessages
+		]
+
+		const chatRequestOpts: CreateChatCompletionRequest = {
+			model: 'gpt-3.5-turbo',
+			messages,
+			temperature: 0.2,
+			stream: true
+		}
+
+		const chatResponse = await fetch('https://api.openai.com/v1/chat/completions', {
+			headers: {
+				Authorization: `Bearer ${OPENAI_KEY}`,
+				'Content-Type': 'application/json'
+			},
+			method: 'POST',
+			body: JSON.stringify(chatRequestOpts)
+		})
+
+		if (!chatResponse.ok) {
+			const err = await chatResponse.json()
+			throw new Error(err.error.message)
+		}
+
+		return new Response(chatResponse.body, {
+			headers: {
+				'Content-Type': 'text/event-stream'
+			}
+		})
+	} catch (err) {
+		console.error(err)
+		return json({ error: 'There was an error processing your request' }, { status: 500 })
+	}
+}
